c8c5d158ec6d1669254cbf4425e0967b5fc0a234
Maria Pilar Guerra Arias <pili@3scale.net>
2014-09-19 13:54:53 +0200

    minor fixes to deployment

--- tools/helper.rb
+++ tools/helper.rb
@@ -2,6 +2,6 @@ require 'rest_client'
 
 class Helper < Sinatra::Base
   get '/success' do
-    RestClient.post "http://apistrat-account-creation.herokuapp.com/create", { :email => ENV['THREESCALE_ADMIN_EMAIL'], :org_name => ENV['ORG_NAME'], :subdomain => ENV['SUBDOMAIN'] }
+    RestClient.post "http://apistrat-account-creation.herokuapp.com/create", { :email => ENV['THREESCALE_ADMIN_EMAIL'], :org_name => ENV['ORG_NAME'], :subdomain => ENV['SUBDOMAIN'] || ENV['ORG_NAME'] }
   end
 end
