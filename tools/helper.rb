13fc632a171684497febcb9bdcf7d3ff1e8915b4
Maria Pilar Guerra Arias <pili@3scale.net>
2014-10-16 20:53:03 +0200

    remove ssl_version

--- tools/helper.rb
+++ tools/helper.rb
@@ -3,7 +3,6 @@ require 'patron'
 class Helper < Sinatra::Base
   get '/success' do
     s = Patron::Session.new
-    s.ssl_version = 'TLSv1'
     s.base_url = 'https://apistrat-account-creation.herokuapp.com:443'
     s.post("/create", :email => ENV['THREESCALE_ADMIN_EMAIL'], :org_name => ENV['ORG_NAME'], :subdomain => ENV['SUBDOMAIN'] || ENV['ORG_NAME'], :password => ENV['THREESCALE_ADMIN_PWD'])
   end
