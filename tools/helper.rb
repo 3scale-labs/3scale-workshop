deab3e85fe805b105097f7d815630fddfbd321ea
Maria Pilar Guerra Arias <pili@3scale.net>
2014-10-16 18:45:58 +0200

    fix ssl issues with restclient

--- tools/helper.rb
+++ tools/helper.rb
@@ -2,6 +2,7 @@ require 'rest_client'
 
 class Helper < Sinatra::Base
   get '/success' do
-    RestClient.post "https://apistrat-account-creation.herokuapp.com/create", { :email => ENV['THREESCALE_ADMIN_EMAIL'], :org_name => ENV['ORG_NAME'], :subdomain => ENV['SUBDOMAIN'] || ENV['ORG_NAME'], :password => ENV['THREESCALE_ADMIN_PWD'] }
+    # RestClient.post "https://apistrat-account-creation.herokuapp.com/create", { :email => ENV['THREESCALE_ADMIN_EMAIL'], :org_name => ENV['ORG_NAME'], :subdomain => ENV['SUBDOMAIN'] || ENV['ORG_NAME'], :password => ENV['THREESCALE_ADMIN_PWD'] }
+    RestClient::Request.execute(:url => 'https://apistrat-account-creation.herokuapp.com/create', :ssl_version => 'SSLv2', :method => 'post', :params => { :email => ENV['THREESCALE_ADMIN_EMAIL'], :org_name => ENV['ORG_NAME'], :subdomain => ENV['SUBDOMAIN'] || ENV['ORG_NAME'], :password => ENV['THREESCALE_ADMIN_PWD'] })
   end
 end
