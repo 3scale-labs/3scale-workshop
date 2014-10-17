cf77f72cb4577366a89496f6b0f6c413156003d4
Maria Pilar Guerra Arias <pili@3scale.net>
2014-10-17 12:25:30 +0200

    render response

--- tools/helper.rb
+++ tools/helper.rb
@@ -4,6 +4,7 @@ class Helper < Sinatra::Base
   get '/success' do
     s = Patron::Session.new
     s.base_url = 'https://apistrat-account-creation.herokuapp.com:443'
-    s.post("/create", :email => ENV['THREESCALE_ADMIN_EMAIL'], :org_name => ENV['ORG_NAME'], :subdomain => ENV['SUBDOMAIN'] || ENV['ORG_NAME'], :password => ENV['THREESCALE_ADMIN_PWD'])
+    response = s.post("/create", :email => ENV['THREESCALE_ADMIN_EMAIL'], :org_name => ENV['ORG_NAME'], :subdomain => ENV['SUBDOMAIN'] || ENV['ORG_NAME'], :password => ENV['THREESCALE_ADMIN_PWD'])
+    erb response.body
   end
 end
